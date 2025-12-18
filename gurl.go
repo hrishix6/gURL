503e5d4d582407bade84bd3cccd78d30d47e853a
hrishix6 <contact@hrishikesh.dev>
2025-12-18 11:05:27 +0530

    expose crud operations as bindings, wire up db initialization in startup

--- gurl.go
+++ gurl.go
@@ -2,136 +2,73 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"gurl/internal"
+	"gurl/internal/db"
+	"gurl/internal/models"
+	"gurl/internal/request"
+	"gurl/internal/response"
+	"gurl/internal/utils"
 	"io"
 	"net/http"
 	"os"
-	"strings"
-	"sync"
+	"path/filepath"
 	"time"
 
 	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"gorm.io/gorm"
 )
 
 type Gurl struct {
-	c     http.Client
-	store *ReqStore
-	ctx   context.Context
+	c              http.Client
+	version        string
+	store          *request.GurlReqContextStore
+	ctx            context.Context
+	tmpDir         string
+	gurlResBuilder *response.GurlResponseBuilder
+	storage        *db.Storage
+	name 			string
 }
 
-func NewGurl() *Gurl {
+func NewGurl(dbConn *gorm.DB, version string) *Gurl {
+	storage := db.NewStorage(dbConn)
 	return &Gurl{
-		c: http.Client{},
-		store: &ReqStore{
-			m: make(map[string]context.CancelFunc),
+		c: http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+			Timeout: 5 * time.Minute,
 		},
+		version:        version,
+		name : fmt.Sprintf("%s_%s", internal.APP_NAME, version),
+		store:          request.NewGurlReqContextStore(),
+		gurlResBuilder: response.NewGurlResponseBuilder(storage),
+		storage:        storage,
 	}
-}
-
-type GurlKeyValItem struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
-type GurlKeyValMultiPartItem struct {
-	Key    string `json:"key"`
-	Value  string `json:"value"`
-	IsFile bool   `json:"isFile"`
-}
-
-type GurlReq struct {
-	Id             string                    `json:"id"`
-	Method         string                    `json:"method"`
-	Url            string                    `json:"url"`
-	BodyType       string                    `json:"bodyType"`
-	Headers        []GurlKeyValItem          `json:"headers"`
-	UrlEncodedForm []GurlKeyValItem          `json:"urlencoded"`
-	MultiPartForm  []GurlKeyValMultiPartItem `json:"multipart"`
-	TextBody       string                    `json:"plaintext"`
-	BinaryFile     string                    `json:"binary"`
-}
-
-type GurlRes struct {
-	Id         string           `json:"id"`
-	StatusCode int              `json:"status"`
-	StatusText string           `json:"statusText"`
-	Success    bool             `json:"success"`
-	Headers    []GurlKeyValItem `json:"headers"`
-	Body       string           `json:"body"`
-	IsFile     bool             `json:"isFile"`
-	SizeBytes  int              `json:"size"`
-	TimeMs     int64            `json:"time"`
-}
-
-func NewGurlRes(id string) *GurlRes {
-	return &GurlRes{
-		Id: id,
-	}
-}
-
-func (gres *GurlRes) WithStatus(t string, code int) *GurlRes {
-	gres.StatusText = t
-	gres.StatusCode = code
-	gres.Success = code >= 200 && code < 300
-	return gres
-}
-
-func (gres *GurlRes) HavingBody(b string) *GurlRes {
-	gres.Body = b
-	return gres
-}
-
-func (gres *GurlRes) WithHeaders(h http.Header) *GurlRes {
-
-	for k, v := range h {
-		gres.Headers = append(gres.Headers, GurlKeyValItem{
-			Key:   k,
-			Value: strings.Join(v, ""),
-		})
-	}
-
-	return gres
-}
-
-func (gres *GurlRes) WithStats(size int, timeMs int64) *GurlRes {
-	gres.SizeBytes = size
-	gres.TimeMs = timeMs
-	return gres
-}
-
-type ReqStore struct {
-	sync.Mutex
-	m map[string]context.CancelFunc
-}
-
-func (rs *ReqStore) AddReq(id string, cancelFn context.CancelFunc) {
-	rs.Lock()
-	defer rs.Unlock()
-	rs.m[id] = cancelFn
-}
-
-func (rs *ReqStore) CancelReq(id string) {
-	cancelFn, ok := rs.m[id]
-
-	if !ok {
-		return
-	}
-
-	cancelFn()
-	delete(rs.m, id)
 }
 
 func (g *Gurl) startup(ctx context.Context) {
 	g.ctx = ctx
+
+	tmpDir, err := utils.InitTempDir(g.name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	g.tmpDir = tmpDir
+	fmt.Printf("tmp location for this app: %s", tmpDir)
+
+	g.storage.Init(ctx, mimedbJson)
 }
 
-type FileStats struct {
-	Name string `json:"name"`
-	Size int64  `json:"size"`
-	Path string `json:"path"`
+func (g *Gurl) shutdown(ctx context.Context) bool {
+	_ = utils.CleanupTempDir(g.tmpDir)
+	return false
 }
 
-func (g *Gurl) ChooseFile() (*FileStats, error) {
+func (g *Gurl) ChooseFile() (*models.FileStats, error) {
 
 	file, err := runtime.OpenFileDialog(g.ctx, runtime.OpenDialogOptions{
 		Title:           "Choose File to Upload",
@@ -152,20 +89,105 @@ func (g *Gurl) ChooseFile() (*FileStats, error) {
 		return nil, fmt.Errorf("chosen item is not a file")
 	}
 
-	return &FileStats{
+	return &models.FileStats{
 		Name: info.Name(),
 		Size: info.Size(),
 		Path: file,
 	}, nil
 }
 
-func (g *Gurl) SendReq(r GurlReq) (*GurlRes, error) {
+func (g *Gurl) SendReq(r models.GurlReq) (*models.GurlRes, error) {
 
 	ctx, cancelFunc := context.WithCancel(context.Background())
 
 	g.store.AddReq(r.Id, cancelFunc)
 
-	req, err := http.NewRequestWithContext(ctx, r.Method, r.Url, nil)
+	fmt.Printf("\nadded req %s to store", r.Id)
+
+	var body io.Reader
+	var cType string
+
+	//set body
+	switch r.BodyType {
+	case "json":
+		v, err := r.ToTextBody()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = "application/json"
+
+	case "xml":
+		v, err := r.ToTextBody()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = "application/xml"
+
+	case "plaintext":
+		v, err := r.ToTextBody()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = "text/plain"
+
+	case "urlencoded":
+		v, err := r.ToUrlEncodedForm()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = "application/x-www-form-urlencoded"
+
+	case "multipart":
+		v, t, err := r.ToMultipartFormData()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = t
+
+	case "binary":
+		v, err := r.ToRawFileUpload()
+
+		if err != nil {
+			return nil, err
+		}
+
+		body = v
+		cType = "application/octet-stream"
+	}
+
+	req, err := http.NewRequestWithContext(ctx, r.Method, r.Url, body)
+
+	//set User Defined headers
+	for _, header := range r.Headers {
+		req.Header.Add(header.Key, header.Value)
+	}
+
+	//if no content-type is set by user add Default
+	if body != nil && req.Header.Get("content-type") == "" {
+		req.Header.Set("content-type", cType)
+	}
+
+	//if no User-Agent set by user add default
+	if req.Header.Get("user-agent") == "" {
+		req.Header.Set("user-agent", g.name)
+	}
+
+	defer g.store.CancelReq(r.Id)
 
 	if err != nil {
 		return nil, err
@@ -183,17 +205,223 @@ func (g *Gurl) SendReq(r GurlReq) (*GurlRes, error) {
 
 	defer res.Body.Close()
 
-	bytes, err := io.ReadAll(res.Body)
+	tmpF, err := os.CreateTemp(g.tmpDir, fmt.Sprintf("gurl-%s", r.Id))
 
 	if err != nil {
 		return nil, err
 	}
 
-	gurlRes := NewGurlRes(r.Id).WithHeaders(res.Header).HavingBody(string(bytes)).WithStatus(res.Status, res.StatusCode).WithStats(len(bytes), end)
+	defer tmpF.Close()
+
+	writtenBytes, err := io.Copy(tmpF, res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("stored response at %s", tmpF.Name())
+
+	gurlRes := g.gurlResBuilder.BuildGurlRes(r.Id, res, tmpF.Name(), writtenBytes, end)
+
+	if gurlRes.Body.IsText {
+		text, err := g.ReadFileText(gurlRes.Body.Filepath)
+
+		if err != nil {
+			return nil, err
+		}
+
+		gurlRes.Body.TextContent = text
+	}
 
 	return gurlRes, nil
 }
 
 func (g *Gurl) CancelReq(id string) {
 	g.store.CancelReq(id)
+}
+
+func (g *Gurl) SaveFile(src string, ext string) error {
+
+	dst, err := runtime.SaveFileDialog(g.ctx, runtime.SaveDialogOptions{
+		Title:           "Choose location to store response",
+		DefaultFilename: filepath.Base(src) + ext,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	srcF, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	dstF, err := os.Create(dst)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(dstF, srcF)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) ReadFileText(src string) (string, error) {
+
+	f, err := os.Open(src)
+
+	if err != nil {
+		return "", err
+	}
+
+	bytes, err := io.ReadAll(f)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+// db
+func (g *Gurl) RemoveReq(id string) error {
+	return g.storage.DeleteReq(id)
+}
+
+func (g *Gurl) FindReqById(id string) (*models.RequestDTO, error) {
+	found, err := g.storage.FindReq(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return found.ToRequestDTO(), nil
+}
+
+func (g *Gurl) AddReq(dto models.AddReqDTO) error {
+	return g.storage.AddReq(&models.Request{
+		Id:           dto.Id,
+		CollectionId: dto.CollectionId,
+	})
+}
+
+func (g *Gurl) UpdateReqUrl(dto models.UpdateReqUrlDTO) error {
+	_, err := g.storage.UpdateReqUrl(dto.RequestId, dto.Url)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqQuery(dto models.UpdateReqQueryDTO) error {
+	_, err := g.storage.UpdateReqQuery(dto.RequestId, dto.QueryJSON)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqHeaders(dto models.UpdateReqHeadersDTO) error {
+
+	_, err := g.storage.UpdateReqHeaders(dto.RequestId, dto.HeadersJSON)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqMultipartForm(dto models.UpdateReqMultipartFormDTO) error {
+	_, err := g.storage.UpdateReqMultipartForm(dto.RequestId, dto.MultipartJSON)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqUrlEncodedForm(dto models.UpdateReqUrlEncodedFormDTO) error {
+	_, err := g.storage.UpdateReqUrlEncodedForm(dto.RequestId, dto.UrlEncodedFormJSON)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqTextBody(dto models.UpdateReqTextBodyDTO) error {
+	_, err := g.storage.UpdateReqTextBody(dto.RequestId, dto.TextBody)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqBinaryBody(dto models.UpdateReqBinaryBodyDTO) error {
+	_, err := g.storage.UpdateReqBinaryBody(dto.RequestId, dto.BinaryBodyJSON)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqMethod(dto models.UpdateReqMethodDTO) error {
+	_, err := g.storage.UpdateReqMethod(dto.RequestId, dto.Method)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqBodyType(dto models.UpdateReqBodyTypeDTO) error {
+	_, err := g.storage.UpdateReqBodyType(dto.RequestId, dto.BodyType)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (g *Gurl) UpdateReqCollection(dto models.UpdateReqCollectionDTO) error {
+	_, err := g.storage.UpdateReqCollection(dto.RequestId, dto.CollectionId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// tabs
+func (g *Gurl) UpdateOpenTabs(dto models.UpdateOpenTabsDTO) error {
+	return g.storage.UpdateOpenTabs(dto.OpenTabsJSON)
+}
+
+func (g *Gurl) GetOpenTabs() (string, error) {
+	r, err := g.storage.GetUIState()
+	if err != nil {
+		return "", err
+	}
+	return string(r.OpenTabs), nil
 }
