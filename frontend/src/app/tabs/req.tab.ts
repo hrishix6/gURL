63157e455c2b84a3cd3f27ad4e78b38704d348a7
hrishix6 <contact@hrishikesh.dev>
2026-01-13 00:49:24 +0530

    feat(milestone v4): tabs section
    - minor visual changes to tab headers
    - add a environment selector dropdown on far right
    - implement tab closure via CTRL+W and MOUSE3 key press.
    - implement layout switching in request tab, user can save preferred layout between 'horizontal, verticle, responsive'

--- frontend/src/app/tabs/req.tab.ts
+++ frontend/src/app/tabs/req.tab.ts
@@ -1,27 +1,23 @@
 import { NgClass } from "@angular/common";
 import {
 	Component,
+	computed,
 	HostBinding,
 	inject,
 	input,
 	type OnInit,
 } from "@angular/core";
-import { AppService, FormService } from "../services";
-import { RequestFormDetails } from "../request/req.form.details";
-import { ReqFormHeader } from "../request/req.form.header";
-import { ResponseDetails } from "../response/res.details";
+import { RequestFormDetails } from "@/request/req.form.details";
+import { ReqFormHeader } from "@/request/req.form.header";
+import { ResponseDetails } from "@/response/res.details";
+import { AppService, FormService } from "@/services";
+import { FormLayout } from "@/types";
 
 @Component({
 	selector: `app-request-tab`,
 	template: `
     <app-req-form-header></app-req-form-header>
-    <div
-      [ngClass]="{
-        'flex-1 flex gap-2 overflow-hidden': true,
-        'flex-col': appSvc.formLayout() === 'h',
-        'flex-row': appSvc.formLayout() === 'v'
-      }"
-    >
+    <div [ngClass]="layoutClass()">
       <app-req-form-details></app-req-form-details>
       <div class="p-px bg-base-100"></div>
       <app-res-details></app-res-details>
@@ -35,18 +31,41 @@ export class RequestTab implements OnInit {
 	tabId = input.required<string>();
 	draftId = input.required<string>();
 
+	layoutClass = computed(() => {
+		const layout = this.appSvc.formLayout();
+		const defaults = ["flex-1 overflow-hidden flex"];
+		switch (layout) {
+			case FormLayout.Horizontal: {
+				defaults.push("flex-col");
+				break;
+			}
+			case FormLayout.Vertical: {
+				break;
+			}
+			case FormLayout.Responsive: {
+				defaults.push("flex-col xl:flex-row");
+				break;
+			}
+		}
+
+		const cls = defaults.join(" ");
+		console.log(`layout class - ${cls}`);
+		return cls;
+	});
+
 	formSvc = inject(FormService);
 	appSvc = inject(AppService);
 
 	@HostBinding("class") get defaultClass() {
 		if (this.activeId() === this.tabId()) {
-			return "flex-1 flex flex-col gap-2 overflow-hidden";
+			return "flex-1 flex flex-col overflow-hidden";
 		}
 
 		return "hidden";
 	}
 
 	ngOnInit(): void {
+		console.log(`called`);
 		this.formSvc.initializeReqForm(this.draftId());
 	}
 }
--- frontend/src/app/tabs/req.tabs.container.ts
+++ frontend/src/app/tabs/req.tabs.container.ts
@@ -6,22 +6,19 @@ import {
 	viewChild,
 } from "@angular/core";
 import { LucideAngularModule, Plus } from "lucide-angular";
-import { TabsService } from "../services";
+import { AppDropdown } from "@/common/components";
+import { AppService, TabsService } from "@/services";
 import { RequestTab } from "./req.tab";
 import { TabHeader } from "./req.tab.header";
 
 @Component({
 	selector: "section[appReqTabs]",
 	template: `
-    <div class="flex items-center relative px-2">
+    <div class="flex items-center relative px-2 py-1">
       <!-- Button to add  new tab -->
       <button class="btn btn-ghost btn-square btn-sm mr-1" (click)="tabsSvc.createFreshTab()">
         <lucide-angular [img]="PlusIcon" class="size-4" />
       </button>
-      <!-- Gradient Shadow to indicate scrollable area -->
-      <div
-        class="pointer-events-none absolute right-0 top-0 h-full w-10 bg-linear-to-r from-transparent to-base-100"
-      ></div>
       <!-- Tabs -->
       <div
         class="flex flex-1 gap-0.5 overflow-x-auto no-scrollbar whitespace-nowrap"
@@ -38,21 +35,38 @@ import { TabHeader } from "./req.tab.header";
         ></div>
         }
       </div>
+      <div class="ml-1 relative">
+          <!-- Gradient Shadow to indicate scrollable area -->
+          <div
+          class="pointer-events-none absolute -left-10 top-0 h-full w-10 bg-linear-to-r from-transparent to-bg-base-100/5"
+        ></div>
+          <app-dropdown
+              [activeItem]="appSvc.activeEnvironment()"
+              [items]="appSvc.environments()"
+              (onItemSelection)="appSvc.setActiveEnvironment($event)"
+              [size]="'md'"
+              [align]="'end'"
+              [direction]="'down'"
+              [varient]="'soft'"
+              >
+          </app-dropdown>
+      </div>
     </div>
     <!-- Tab content view -->
     @for (tab of tabsSvc.openTabs(); track tab.id) { @switch (tab.entityType) { @case ("req") {
-    <app-request-tab
-      [tabId]="tab.id"
-      [draftId]="tab.entityId"
-      [activeId]="tabsSvc.activeTab()"
-    ></app-request-tab>
+            <app-request-tab
+              [tabId]="tab.id"
+              [draftId]="tab.entityId"
+              [activeId]="tabsSvc.activeTab()"
+        ></app-request-tab>
     } } }
   `,
-	imports: [LucideAngularModule, TabHeader, RequestTab],
+	imports: [LucideAngularModule, TabHeader, RequestTab, AppDropdown],
 })
 export class AppTabsWrapper {
 	readonly PlusIcon = Plus;
 	readonly tabsSvc = inject(TabsService);
+	readonly appSvc = inject(AppService);
 
 	@HostBinding("class")
 	def = "flex-1 flex flex-col overflow-hidden";
