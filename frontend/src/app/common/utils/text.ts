cef95da32356ab3d8f634d02e5a9ca94df2b3707
hrishix6 <contact@hrishikesh.dev>
2026-02-28 00:19:58 +0530

    feat(code-editor): add a code editor with highlighting support with help of codemirror package

--- frontend/src/app/common/utils/text.ts
+++ frontend/src/app/common/utils/text.ts
@@ -3,46 +3,46 @@ import { nanoid } from "nanoid";
 import { getHttpExecutor } from "@/services";
 
 export function parseTextAsKeyVal(text: string) {
-    const items: models.GurlKeyValItem[] = [];
-    for (const line of text.split("\n")) {
-        if (line.length && line.includes(":")) {
-            let [key, val] = line.split(":");
-            if (key) {
-                let enabled = "on";
-                if (key.startsWith("#")) {
-                    enabled = "off";
-                    key = key.slice(1);
-                }
-                items.push({
-                    id: nanoid(),
-                    key: key,
-                    enabled,
-                    val,
-                });
-            }
-        }
-    }
-    return Promise.resolve(items);
+	const items: models.GurlKeyValItem[] = [];
+	for (const line of text.split("\n")) {
+		if (line.length && line.includes(":")) {
+			let [key, val] = line.split(":");
+			if (key) {
+				let enabled = "on";
+				if (key.startsWith("#")) {
+					enabled = "off";
+					key = key.slice(1);
+				}
+				items.push({
+					id: nanoid(),
+					key: key,
+					enabled,
+					val,
+				});
+			}
+		}
+	}
+	return Promise.resolve(items);
 }
 
 export async function parseTextAsCookies(
-    text: string,
+	text: string,
 ): Promise<models.GurlKeyValItem[]> {
-    try {
-        const httpExec = getHttpExecutor();
-        const results = await httpExec.parseCookieRaw(text);
-        if (Array.isArray(results)) {
-            const parsed = results.map((x) => ({
-                id: nanoid(),
-                key: x.key,
-                val: x.val,
-                enabled: x.enabled,
-            }));
+	try {
+		const httpExec = getHttpExecutor();
+		const results = await httpExec.parseCookieRaw(text);
+		if (Array.isArray(results)) {
+			const parsed = results.map((x) => ({
+				id: nanoid(),
+				key: x.key,
+				val: x.val,
+				enabled: x.enabled,
+			}));
 
-            return parsed;
-        }
-        return [];
-    } catch (_error) {
-        return [];
-    }
+			return parsed;
+		}
+		return [];
+	} catch (_error) {
+		return [];
+	}
 }
